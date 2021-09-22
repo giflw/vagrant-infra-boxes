ef1d9474d444ed4ea20aa8b9a5283caccaab275d
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2021-09-22 03:13:38 -0300

    Finish rockylinux/sqlsrv-php-apache

--- build.sh
+++ build.sh
@@ -5,22 +5,48 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 
 function build_box {
+    envvars="`jq -r .env info.json`"
+    if [ -n "$envvars" ]; then
+        set -a # -o allexport
+        eval "$envvars"
+        echo "envfile --------------------------"
+        echo "$envvars"
+        echo "----------------------------------"
+        set +a # +o allexport
+    fi
+
     name=$(basename `pwd`)
+    name=`jq .os -r info.json | cut -f 1 -d -`-${name}
     echo "Box"
     echo "Name:         $name"
     echo "Directory:    `pwd`"
-    cat <<EOF > info.json
-{
-    "author": "${AUTHOR:=Guilherme I F L Weizenmann}",
-    "homepage": "${HOMEPAGE:=https://github.com/giflw/vagrant-infra-boxes/blob/main/${name}}"
-}
-EOF
-    #vagrant up
-    #vagrant halt
-    vagrant package --info info.json --output "${name}.box" --vagrantfile Vagrantfile
 
-    # vars
-    grep ENV Vagrantfile | tr ' ' $'\n' | xargs -L 1 | grep ENV | tr '[]' ' ' | awk '{print $2}' | sort -u
+    vagrant up
+
+    vagrant halt
+
+    if [ -f "${name}.box" ]; then
+        rm "${name}.box"
+    fi
+
+    vagrant package \
+        --info info.json \
+        --output "${name}.box" \
+        --vagrantfile Vagrantfile
+
+    vagrant cloud publish \
+        giflw/${name} \
+        "`jq .os -r info.json | cut -f 2 -d -`-`date +%Y%m%d%H%M`" \
+        virtualbox \
+        ./${name}.box \
+        --version-description "`jq .description -r info.json`" \
+        --release \
+        --no-private \
+        --force
+
+    vagrant destroy -f
+
+    rm "${name}.box"
 }
 
 
@@ -34,6 +60,7 @@ for distro in `ls -d */`; do
             echo '--------------------------------------------------'
             echo $box
             cd $box
+            pwd
             build_box
             echo '--------------------------------------------------'
         done
